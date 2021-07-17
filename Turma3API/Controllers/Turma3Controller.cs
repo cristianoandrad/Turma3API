5e704ffa22df5c79c7443c489bcf5f825ddd49ab
cristianoandrad <cristianocontabil@gmail.com>
2021-07-17 18:22:11 -0300

    Inicial

---
+++ Turma3API/Controllers/Turma3Controller.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turma3API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Turma3Controller : ControllerBase
+    {
+        [HttpPost]
+
+        public ActionResult Post()
+        {
+            return Ok("Hello Post");
+        }
+
+        [HttpGet]
+
+        public ActionResult Get()
+        {
+            return Ok("Hello Get");
+        }
+
+        [HttpPut]
+
+        public ActionResult Put()
+        {
+            return Ok("Hello Put");
+        }
+
+        [HttpPatch]
+
+        public ActionResult Patch()
+        {
+            return Ok("Hello Patch");
+        }
+
+        [HttpDelete]
+
+        public ActionResult Delete()
+        {
+            return Ok("Hello Delete");
+        }
+
+        [HttpHead]
+
+        public ActionResult Head()
+        {
+            return Ok("Hello Head");
+        }
+
+
+    }
+}
